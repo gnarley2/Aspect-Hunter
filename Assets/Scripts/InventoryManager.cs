eb06c64a9dcc571a80f393ae30043b8a302e5e8b
gnarley2 <112232940+gnarley2@users.noreply.github.com>
2024-03-30 21:55:14 +1100

    InventoryManager holds a list of the items collected

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance;
     public List<Item> Items = new List<Item>();
 
+    public Transform ItemContent;
+    public GameObject InventoryItem;
+
+    private Text itemName;
+    private Image itemIcon;
     private void Awake()
     {
         Instance = this;
@@ -15,6 +22,8 @@ public class InventoryManager : MonoBehaviour
     public void Add(Item Item)
     {
         Items.Add(Item);
+       // ListItems();
+        //debug messages to just check if adding correctly
         Debug.Log("Item Added to Inventory");
         string TotalInventory = "";
         foreach (Item i in Items)
@@ -28,11 +37,20 @@ public class InventoryManager : MonoBehaviour
                 TotalInventory += ", " + i.itemName;
             }
         }
-        Debug.Log("Inventory - " + TotalInventory); // Assuming Item has a property called Name
+        Debug.Log("Inventory - " + TotalInventory); 
     }
 
     public void Remove(Item Item)
     {
         Items.Remove(Item);
     }
+
+
+    public void ListItems()
+    {
+
+
+     
+    }
 }
+
