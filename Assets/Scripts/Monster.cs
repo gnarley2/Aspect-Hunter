005443723732c0b17d55a660e22368d166860e58
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-03-30 17:59:41 +1100

    adding simple monster behaviour

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -11,8 +11,10 @@ public class Monster : MonoBehaviour
 
     private Vector2 currentDestination;
     private Vector2 startPos;
+    private bool isIdle = false;
 
     private Rigidbody2D rb;
+
 
     private void Awake()
     {
@@ -34,14 +36,26 @@ public class Monster : MonoBehaviour
 
     void Move()
     {
+        if (isIdle) return;
+        
         if (Vector2.Distance(transform.position, currentDestination) < 0.1f)
         {
-            GetRandomDestination();
+            StartCoroutine(Idle());
         }
         else
         {
             rb.velocity = (currentDestination - (Vector2)transform.position).normalized * speed * Time.fixedDeltaTime;
         }
+    }
+    
+    IEnumerator Idle()
+    {
+        isIdle = true;
+        rb.velocity = Vector2.zero;
+        yield return new WaitForSeconds(2f);
+
+        isIdle = false;
+        GetRandomDestination();
     }
 
     private void OnDrawGizmosSelected()
