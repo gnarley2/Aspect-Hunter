78decfad34e86cb77915100c81066ebc4105ab0d
N1ckm4n <nicholas.danaskos@gmail.com>
2024-04-01 21:13:55 +1100

    inventory working

--- Assets/Scripts/Items/InventoryManager.cs
+++ Assets/Scripts/Items/InventoryManager.cs
@@ -11,17 +11,19 @@ public class InventoryManager : MonoBehaviour
     public Dictionary<Item, int> Items = new Dictionary<Item, int>();
     public GameObject player;
     public GameObject lanternPrefab;
+    public Image[] itemIcons;
 
     private int numberOfLanterns = 0;
-
-    private void Update()
-    {
-
-    }
 
     private void Awake()
     {
         Instance = this;
+        Transform itemsParent = GameObject.Find("itemsParent").transform;
+        itemIcons = new Image[itemsParent.childCount];
+        for (int i = 0; i < itemsParent.childCount; i++)
+        {
+            itemIcons[i] = itemsParent.GetChild(i).Find("itembutton/icon").GetComponent<Image>();
+        }
     }
 
     public void Add(Item item)
@@ -35,7 +37,7 @@ public class InventoryManager : MonoBehaviour
             Items.Add(item, 1);
         }
 
-
+        UpdateInventoryUI(item.icon);
 
         Debug.Log("Inventory Items:");
         foreach (KeyValuePair<Item, int> entry in Items)
@@ -50,9 +52,18 @@ public class InventoryManager : MonoBehaviour
             GameObject lantern = Instantiate(lanternPrefab, player.transform);
             lantern.transform.localPosition = new Vector3(0, 0, 0); // Adjust the position as needed
         }
+    }
 
-
-
+    void UpdateInventoryUI(Sprite icon)
+    {
+        foreach (var itemIcon in itemIcons)
+        {
+            if (itemIcon.sprite == null) // This finds the first empty slot
+            {
+                itemIcon.sprite = icon;
+                break; // Exit the loop after setting the icon
+            }
+        }
     }
 
     public void Remove(Item item)
