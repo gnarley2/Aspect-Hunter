549080408016070bf69307bc4e0d378197cd7835
gnarley2 <112232940+gnarley2@users.noreply.github.com>
2024-04-10 00:30:00 +1000

    Melee Update
    Changed to a different melee object with an animation

--- Assets/Scripts/Player/MeleeMovement.cs
+++ Assets/Scripts/Player/MeleeMovement.cs
@@ -7,15 +7,14 @@ public class MeleeMovement : MonoBehaviour
 {
     private Vector3 direction;
     private Transform playerTransform;
+
     [SerializeField] public float radius = 2f; // Radius of the arc
     [SerializeField] public float speed = 20f; // Speed of movement along the arc
     [SerializeField] private float maxAngle = 20f;
-    private float angle = 0f; // Current angle on the circle
-  
-    private float initialAngle = 0f; // Initial angle for the arc
 
+    private float angle = 0f; // Current angle on the circle
+    private float initialAngle = 0f; // Initial angle for the arc
     private int damage;
-    
 
     public void Initialize(Transform playerTransform, Vector3 initialDirection, int damage)
     {
@@ -23,7 +22,6 @@ public class MeleeMovement : MonoBehaviour
         SetInitialDirection(initialDirection);
         this.damage = damage;
     }
-    
 
     // Update is called once per frame
     void Update()
@@ -41,22 +39,25 @@ public class MeleeMovement : MonoBehaviour
     {
         angle += speed * Time.deltaTime;
         float currentAngle = initialAngle + angle;
-        float x = Mathf.Cos(currentAngle * Mathf.Deg2Rad) * radius;
-        float y = Mathf.Sin(currentAngle * Mathf.Deg2Rad) * radius;
-        transform.position = playerTransform.position + new Vector3(x, y, 0f);
+        float x = playerTransform.position.x + radius * Mathf.Cos(currentAngle * Mathf.Deg2Rad);
+        float y = playerTransform.position.y + radius * Mathf.Sin(currentAngle * Mathf.Deg2Rad);
+        transform.position = new Vector3(x, y, 0f);
+
+        // Rotate the sprite based on the angle from the center
+        float spriteAngle = Mathf.Atan2(y - playerTransform.position.y, x - playerTransform.position.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(spriteAngle, Vector3.forward);
 
         if (angle >= maxAngle)
         {
             Destroy(gameObject); // Destroy the melee object
         }
     }
+
     public void SetInitialDirection(Vector3 initialDirection)
-    { 
+    {
         initialAngle = Mathf.Atan2(initialDirection.y, initialDirection.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.LookRotation(Vector3.forward, initialDirection);
-
     }
-
 
     private void OnTriggerEnter2D(Collider2D other)
     {
