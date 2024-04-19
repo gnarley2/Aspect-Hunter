4fce09702a2759b7626b0b6fb6c85d50f7ad5170
gnarley2 <112232940+gnarley2@users.noreply.github.com>
2024-04-19 19:32:38 +1000

    More light prefab chanbges

--- Assets/Scripts/Items and Inventory/EquippableItem.cs
+++ Assets/Scripts/Items and Inventory/EquippableItem.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using Kryz.CharacterStats;
+using UnityEditor;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+using Transform = UnityEngine.Transform;
 
 public enum EquipmentType
 {
@@ -18,10 +21,32 @@ public enum EquipmentType
 public class EquippableItem : Item
 {
     public EquipmentType EquipmentType;
+    public GameObject LanternBugPrefab;
+    public GameObject LanternPrefab;
+    public GameObject FlashLightPrefab;
 
     public void Equip(InventoryManager c)
     {
         //add code for buffs
+        if (EquipmentType == EquipmentType.LanternBug && LanternBugPrefab != null)
+        { 
+            Instantiate(LanternBugPrefab);
+
+        }
+        if (EquipmentType == EquipmentType.Lantern && LanternPrefab != null)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+  
+            Instantiate(LanternPrefab, player.transform.position, player.transform.rotation);
+        }
+        if (EquipmentType == EquipmentType.FlashLight && FlashLightPrefab != null)
+        {
+          Instantiate(FlashLightPrefab);
+
+        }
+
+
+
     }
 
     public void Unequip(InventoryManager c)
---
+++ Assets/lantern.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lantern : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     //  GameObject player = GameObject.FindWithTag("Player");
+     //  transform.position = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
